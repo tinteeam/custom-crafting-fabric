0a0e751da1a9731e31b45040986c428570f36fe5
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-19 19:03:00 +0300

    add item  without any texture

--- src/main/java/com/tinteeam/custom_crafting/Customcrafting.java
+++ src/main/java/com/tinteeam/custom_crafting/Customcrafting.java
@@ -1,6 +1,10 @@
 package com.tinteeam.custom_crafting;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback.Registry;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +14,13 @@ public class Customcrafting implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("custom-crafting");
-
+	public static final Item BLANK_DIAMOND = new Item(new FabricItemSettings());
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("custom_crafting:blank_diamond"), CUSTOM_ITEM);
 	}
 }
