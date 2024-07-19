b747966f6ec1efb1ae686277571840c0f3b4ac4b
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-19 17:48:25 +0300

    initial commit

---
+++ src/main/java/com/tinteeam/custom_crafting/CustomcraftingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tinteeam.custom_crafting;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomcraftingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
