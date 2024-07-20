628ec67ee6258bfb5ece4f43c10885e2d79d7314
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-20 10:19:12 +0300

    publish the mod

--- src/main/java/com/tinteeam/custom_crafting/Customcrafting.java
+++ src/main/java/com/tinteeam/custom_crafting/Customcrafting.java
@@ -1,22 +1,18 @@
 package com.tinteeam.custom_crafting;
 
+import com.tinteeam.custom_crafting.item.blank_diamond;
+
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.registry.*;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Customcrafting implements ModInitializer {
 	
 	
 
+
 	@Override
 	public void onInitialize() {
 		blank_diamond.initialize();
 	}
+	//dummy variable
+	 public String MODID = "custom_crafting";
 }
--- src/main/java/com/tinteeam/custom_crafting/item/blank_diamond.java
+++ src/main/java/com/tinteeam/custom_crafting/item/blank_diamond.java
@@ -1,14 +1,15 @@
-package com.tinteeam.custom_crafting;
+package com.tinteeam.custom_crafting.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class blank_diamond {
-    public static final Item BLANK_DIAMOND = register(new Item(new FabricItemSettings()), "blank_diamond");
+    public static final Item BLANK_DIAMOND = register("blank_diamond",new Item(new FabricItemSettings().group(ItemGroup.MISC)), "blank_diamond");
 
-    public static Item register(Item instance, String path) {
+    public static Item register(String name, Item instance, String path) {
         return Registry.register(Registry.ITEM, new Identifier("custom_crafting", "blank_diamond"), instance);
     }
 
